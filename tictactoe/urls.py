706d18847873d4e570dff6fafbc4949a64c8b2b5
MaxwellGraves <maxwell.graves@gmail.com>
2021-08-05 16:05:53 -0400

    It works! Mostly...

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.tictactoe, name="tictactoe"),
+    path("square-click/", views.square_click, name="square-click"),
 ]
--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,5 +1,56 @@
 from django.shortcuts import render
+from random import randrange
 
 # Create your views here.
 def tictactoe(request):
-    return render(request, "tictactoe/tictactoe.html", {})
+    board = [""] * 9
+    with open("board.txt", "w") as f:
+        f.write(" ".join(board))
+    return square_click(request)
+
+
+def square_click(request):
+    with open("board.txt", "r") as f:
+        board = f.readline().split(" ")
+    for i in range(9):
+        if "box" + str(i) in request.POST and board[i] == "":
+            board[i] = "X"
+    winCheck = detectWin(board)
+    if winCheck == "":
+        board = makeMove(board)
+    winCheck = detectWin(board)
+    with open("board.txt", "w") as f:
+        f.write(" ".join(board))
+
+    boardContext = {"sq" + str(i): board[i] for i in range(9)}
+    boardContext["win"] = winCheck
+    return render(request, "tictactoe/tictactoe.html", boardContext)
+
+
+def detectWin(board):
+    # Check Rows
+    sets = [
+        (0, 1, 2),
+        (3, 4, 5),
+        (6, 7, 8),
+        (0, 3, 6),
+        (1, 4, 7),
+        (2, 5, 8),
+        (0, 4, 8),
+        (2, 4, 6),
+    ]
+    for i, j, k in sets:
+        a, b, c = board[i], board[j], board[k]
+        if (a == "X" or a == "O") and a == b and a == c:
+            return a
+    return ""
+
+
+def makeMove(board):
+    empties = [i for i in range(9) if board[i] == ""]
+    print(empties)
+    if len(empties) == 0:
+        return board
+    index = randrange(len(empties))
+    board[empties[index]] = "O"
+    return board
